535d87b259b99192e5281ab3adf58d1ea44b4444
Brandon Haines <haines.brandon.91@gmail.com>
2018-03-20 07:36:34 -0700

    got it functioning

--- server.js
+++ server.js
@@ -26,16 +26,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/sportsScraperdb", {
-  useMongoClient: true
-});
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+  });
+  
 
 // Routes
 
-// A GET route for scraping the echojs website
+// A GET route for scraping the website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
   axios.get("http://www.bleacherreport.com/").then(function(response) {
@@ -43,16 +46,15 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("a.articleTitle").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
+        .children()
         .text();
       result.link = $(this)
-        .children("a")
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
